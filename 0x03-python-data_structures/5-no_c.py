644a73a2e8a74f25ab1d2f566b4e0065a9454225
my_name <fuadnuru45@gmail.com>
2024-02-28 10:57:04 +0300

    FIRST COMMIT

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 5-no_c.py
-# Kobo Highschool <375@kobohighschool.com>
-
-
-def no_c(my_string):
-    """Replace all characters c and c from a string."""
-    copy = [x for x in my_string if x != 'c' and x != 'C']
-    return ("".join(copy))
